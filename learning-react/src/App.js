6c23075a45e4b56ac9377d6e640182c1f6fb7af1
DJKramer314 <djkramer314@gmail.com>
2022-06-23 15:49:55 -0500

    Installed Bootstrap + added a basic navbar element

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationView from './components/NavigationView'
 
 function App() {
   return (
-    <h1>Test Commit</h1>
+    <NavigationView />
   );
 }
 
---
+++ learning-react/src/components/NavigationView.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+function NavigationView() {
+  return (
+    <div class="sample-navbar">
+      <h2 class="sample-navbar-text">Sample Navbar</h2>
+    </div>
+  );
+}
+
+export default NavigationView;
