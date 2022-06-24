4cdf3d707d48c5d4c7c64477ff0427d1e78046cf
DJKramer314 <djkramer314@gmail.com>
2022-06-24 10:29:37 -0500

    Comments added

--- learning-react/src/components/StocksView.jsx
+++ learning-react/src/components/StocksView.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import StockRow from './StockRow';
 
 function StocksView() {
+  //'State' area where the variables update the view
   const [stocksList, updateStocksList] = useState([
-    { ticker: 'ETH', price: 20500, percent: 4.43 },
+    { ticker: 'ETH', price: 1100, percent: 4.43 },
     { ticker: 'AAPL', price: 157, percent: -2.15 },
+    { ticker: 'BTC', price: 20500, percent: -11.12 },
   ]);
 
+  //Arrow function below dictates that the keyword 'stock' represents the iterated items
   return stocksList.map((stock) => (
-    // Below 'key' is necessary for each list item to be unique in the 'props' area of the StockRow file
+    // Below 'key' is necessary for each list item to be unique in the 'props' area of the StockRow file to ensure optimization when rendering
 
     <div className="container" key={stock.ticker}>
       <StockRow
