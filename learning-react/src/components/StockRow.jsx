9f753cca025dff0ec65d551ab6254b6eabffc020
DJKramer314 <djkramer314@gmail.com>
2022-06-24 10:24:25 -0500

    Added some styling to the elements including a conditional color, and added a percentChange prop to each stock

--- learning-react/src/components/StockRow.jsx
+++ learning-react/src/components/StockRow.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 function StockRow(props) {
   return (
-    <div className="row">
-      <div className="col-md-6">
+    <div className={props.percentChange >= 0 ? 'row row-green' : 'row row-red'}>
+      <div className="col-md-3">
         <h1>{props.ticker}</h1>
+      </div>
+      <div className="col-md-3 percent">
+        <h1>{props.percentChange}%</h1>
       </div>
       <div className="col-md-6">
         <h1>Current Price: ${props.price}</h1>
--- learning-react/src/components/StocksView.jsx
+++ learning-react/src/components/StocksView.jsx
@@ -3,15 +3,19 @@ import StockRow from './StockRow';
 
 function StocksView() {
   const [stocksList, updateStocksList] = useState([
-    { ticker: 'BTC', price: 21000 },
-    { ticker: 'AAPL', price: 150 },
+    { ticker: 'ETH', price: 20500, percent: 4.43 },
+    { ticker: 'AAPL', price: 157, percent: -2.15 },
   ]);
 
   return stocksList.map((stock) => (
     // Below 'key' is necessary for each list item to be unique in the 'props' area of the StockRow file
 
     <div className="container" key={stock.ticker}>
-      <StockRow ticker={stock.ticker} price={stock.price} />
+      <StockRow
+        ticker={stock.ticker}
+        price={stock.price}
+        percentChange={stock.percent}
+      />
     </div>
   ));
 }
